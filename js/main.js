10794cfb393c5bf232e057aae2cd9c397d9fd8fc
Chrystelle Poulet <chrystelle.poulet@hetic.net>
2018-02-24 17:57:05 +0100

    all setTimeout() removed

--- js/main.js
+++ js/main.js
@@ -2,23 +2,31 @@
 
 var logo = document.querySelector('.main__logo-container');
 var firstParagraphContent = document.querySelectorAll('.main__first-paragraph-container__content');
-var textContainer = document.querySelector('.main__first-paragraph-container');
+var firstParagraphContainer = document.querySelector('.main__first-paragraph-container');
+var firstParagraphLastChild = document.querySelector('.main__first-paragraph-container p:last-child');
 var delay;
-var i = 0.3;
+var delayDuration;
+var i = 0;
 
 window.addEventListener("load", function(event) {
     logo.classList.add('main__logo-container__move');
     firstParagraphContent.forEach(function(firstParagraphSentence) {
-      delay = i/6;
-      firstParagraphSentence.style.animationDelay = delay + 's';
+      delay = 0.3;
+      delayDuration = delay * i;
+      firstParagraphSentence.style.animationDelay = delayDuration + 's';
       firstParagraphSentence.classList.add('main__first-paragraph-container__content-move');
       i++;
     })
 });
 
-// -------------- text 01 ----------------- //
-// set variables
-
+logo.addEventListener('animationstart', function() {
+  firstParagraphContent.forEach(function(firstParagraphSentence){
+    firstParagraphSentence.classList.add('hidden');
+    firstParagraphSentence.addEventListener('transitionend', function() {
+      firstParagraphContainer.style.display = 'none';
+    })
+  })
+})
 
 logo.addEventListener('animationstart', function() {
   firstParagraphContent.forEach(function(firstParagraphSentence){
@@ -26,16 +34,29 @@ logo.addEventListener('animationstart', function() {
   })
 })
 
-// sentences scrolling horizontally one by one
+var secondParagraphContent = document.querySelectorAll('.main__second-paragraph-container__content');
+var secondParagraphContainer = document.querySelector('.main__second-paragraph-container');
+var secondParagraphLastChild = document.querySelector('.main__second-paragraph-container p:last-child');
 
+firstParagraphLastChild.addEventListener('transitionend', function() {
+  firstParagraphContainer.style.display = 'none';
+  secondParagraphContainer.style.display = 'block';
+  i = 0;
+  secondParagraphContent.forEach(function(secondParagraphSentence) {
+    delay = 0.3;
+    delayDuration = delay * i;
+    secondParagraphSentence.style.animationDelay = delayDuration + 's';
+    secondParagraphSentence.classList.add('main__second-paragraph-container__content-move');
+    i++;
+  })
+})
 
+secondParagraphLastChild.addEventListener('animationend', function() {
+  secondParagraphContent.forEach(function(secondParagraphSentence){
+    secondParagraphSentence.classList.add('hidden');
+  })
+})
 
-// fade out animation
-
-
-
-// set diplay='none' on the text container to avoid blank
-
-/*setTimeout(function() {
-  textContainer.style.display = 'none';
-}, 2250);*/
+secondParagraphLastChild.addEventListener('transitionend', function() {
+  secondParagraphContainer.style.display = 'none';
+})
